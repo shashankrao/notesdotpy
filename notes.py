aa1c0faa39c60f61d90aa657ccfab2428b225041
Shashank Rao <rshashank192@gmail.com>
2014-03-06 20:56:40 +0530

    added line numbers and delete

--- notes.py
+++ notes.py
@@ -13,12 +13,29 @@ fstr = parser.get('files', 'notefile')
 fname = os.path.join(dirstr, fstr)
 
 if len(sys.argv) == 1:
+    from subprocess import call
+    call(["cat", "-n",fname])
+
+elif sys.argv[1]=="--delete" and len(sys.argv)>=3:
     try:
-        f = open(fname, 'rU')
-        shutil.copyfileobj(f, sys.stdout)
-        f.close()
-    except IOError:
-        pass
+        line_to_delete = int(sys.argv[2])
+    except:
+        sys.exit(1)
+    f = open(fname,"r")
+    lines = f.readlines()
+    f.close()
+    if line_to_delete <=0 or line_to_delete > len(lines):
+        print "[ERROR] Invalid Note Number:", line_to_delete
+        sys.exit(1)
+    f = open(fname,"w")
+    line_number = 1;
+    for line in lines:
+        if line_number!=line_to_delete:
+            f.write(line)
+        line_number=line_number+1    
+    f.close()
+    print "[INFO] Note Deleted"	
+
 else:
     f = open(fname, 'a')
     timestr = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M")
